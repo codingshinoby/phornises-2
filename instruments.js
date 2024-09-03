2fd6c2e580885c316f24ab9735fecb52087fa4f5
andrew.lepekha <andrew.lepekha@rebilly.com>
2024-09-03 12:08:05 -0700

    Add deposit prop

--- instruments.js
+++ instruments.js
@@ -1,35 +1,31 @@
 window.onload = function () {
+    // const data = {
+    //     primaryAddress: {
+    //         firstName: 'John',
+    //         lastName: 'Doe',
+    //         emails: [{
+    //             label: 'main',
+    //             value: 'john.doe+test@grr.la',
+    //             primary: true
+    //         }],
+    //     }
+    // };
+    //
+    // const firstCustomer = await api.customers.create({data});
+
+
     RebillyInstruments.mount({
+        deposit: {
+            currency: 'USD',
+            amount: 5.99
+        },
+        jwt: 'jwt_01J6WMHN0E43B0SMBNT92MG454',
         publishableKey: 'pk_sandbox_8BvWrLbeYLapt5CwsZHmQvOdD24ixpAT5TdU7iX',
         organizationId: 'phronesis---dream-team',
         websiteId: 'phronesis-training.com',
         apiMode: 'sandbox',
         summary: '.rebilly-instruments-summary',
         form: '.rebilly-instruments',
-        items: [
-            {
-                planId: 'premium-membership',
-                quantity: 1
-            },
-        ],
-        bumpOffer: [
-            {
-                planId: 'platinum-membership',
-                quantity: 1
-            }
-        ],
-        paymentInstruments: {
-            address: {
-                show: ["email", "phoneNumber", "city", "country"],
-                require: ["address", "email", "phoneNumber", "city", "country"],
-            }
-        },
-        theme: {
-            colorPrimary: "#FFCAD4",
-            colorDanger: "#F5002D",
-            colorText: "#0010F5",
-            buttonColorText: "#F58F00"
-        }
     });
     RebillyInstruments.on('instrument-ready', (instrument) => {
         console.info('instrument-ready', instrument);
