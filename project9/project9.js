5a61b374392febb37b513384113e91da522d4b1d
andrew.lepekha <andrew.lepekha@rebilly.com>
2024-10-07 16:44:40 -0700

    Restruct project

---
+++ project9/project9.js
@@ -0,0 +1,48 @@
+window.onload = function () {
+
+    const consentToSubscription = 'Subscription billing policy.';
+    const agreeToTOS = 'By subscribing, I have read and understood the [terms of service](https://google.com) and [Privacy Policy](https://google.com).';
+
+    RebillyInstruments.mount({
+        publishableKey: 'pk_sandbox_8BvWrLbeYLapt5CwsZHmQvOdD24ixpAT5TdU7iX',
+        organizationId: 'phronesis---dream-team',
+        websiteId: 'phronesis-training.com',
+        apiMode: 'sandbox',
+        items: [
+            {
+                planId: 'premium-membership',
+                quantity: 1
+            },
+        ],
+        paymentInstruments: {
+            address: {
+                show: ["email", "phoneNumber", "city", "country"],
+                require: ["address", "email", "phoneNumber", "city", "country"],
+            }
+        },
+        theme: {
+            colorPrimary: "#FFCAD4",
+            colorDanger: "#F5002D",
+            colorText: "#0010F5",
+            buttonColorText: "#F58F00"
+        },
+        features: {
+            showConsentCheck: ['form'],
+        },
+        i18n: {
+            en: {
+                consentCheck: {
+                    consentToSubscription,
+                    agreeToTOS,
+                },
+            },
+        },
+    });
+    RebillyInstruments.on('instrument-ready', (instrument) => {
+        console.info('instrument-ready', instrument);
+    });
+    RebillyInstruments.on('purchase-completed', (purchase) => {
+        console.info('purchase-completed', purchase);
+    });
+
+}
