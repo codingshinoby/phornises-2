4d5070b5bc6c0657b018ef98d437cd0f7dd37b96
andrew.lepekha <andrew.lepekha@rebilly.com>
2024-10-26 11:11:18 -0700

    Fx

--- project12/server.js
+++ project12/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const RebillyAPI = require("rebilly-js-sdk").default;
 const app = express();
-const port = 8080;
+const port = 80;
 
 app.use(express.static("client"));
 app.use(express.json());
@@ -95,6 +95,6 @@ app.post("/deposit-request", async function (req, res) {
     res.send(response);
 });
 
-app.listen(port, '127.0.0.1', () => {
+app.listen(port, () => {
     console.log(`Sandbox listening on port ${port}`);
 });
