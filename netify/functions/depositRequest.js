29112100ffad0d8b80087b87621457daecd94b24
andrew.lepekha <andrew.lepekha@rebilly.com>
2024-10-26 12:00:41 -0700

    FX

--- netify/functions/depositRequest.js
+++ netify/functions/depositRequest.js
@@ -1,4 +1,11 @@
 const RebillyAPI = require("rebilly-js-sdk").default;
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(express.static("client"));
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 exports.handler = async (event, context) => {
 
